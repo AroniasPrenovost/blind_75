7175b9944baf8bbb640a608ddca71c4c8b88530c
Aron Prenovost <Aroniascoder@gmail.com>
2022-09-16 17:14:26 -0600

    Update binary_tree_traversals.js

--- tree/binary_tree_traversals.js
+++ tree/binary_tree_traversals.js
@@ -5,12 +5,156 @@
 //////////////////////////
 
 
+// v1 (slow)
+
+var inorderTraversal = function(root) {
+    let result = [];
+    dfs(root);
+    
+    function dfs(root) {
+        if(root != null) {
+            dfs(root.left);
+            result.push(root.val);
+            dfs(root.right);
+        }
+    }
+    return result;
+};
+
+inorderTraversal([]);           // []
+inorderTraversal([1]);          // [1]
+inorderTraversal([1,null,2,3]); // [1, 3, 2]
+
+
+
+// v2
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    var stack = []
+    function traver(root){
+        if(!root) return 
+        traver(root.left)
+        stack.push(root.val)
+        traver(root.right)
+    }
+    traver(root)
+    return stack
+};
+
+inorderTraversal([]);           // []
+inorderTraversal([1]);          // [1]
+inorderTraversal([1,null,2,3]); // [1, 3, 2]
+
+// v3
+
+var inorderTraversal = function(root) {
+    let result = [];
+
+    const traverse = node => {
+      if (!node) {
+          return;
+      }
+      
+      if (node.left) [
+        traverse(node.left);
+      }
+
+      result.push(node.val)
+
+      if (node.right) {
+          traverse(node.right)
+      }
+
+    }
+
+    traverse(root)
+    return result
+};
+
+
+
+
 
 //////////////////////////
 //
 // Binary Tree preorder Traversal
 //
 //////////////////////////
+
+
+// v1 
+
+var preorderTraversal = function(root) {
+    let result = [];
+    dfs(root);
+    
+    function dfs(root) {
+        if(root != null){
+            result.push(root.val);
+            dfs(root.left);
+            dfs(root.right);
+        }
+    }
+    return result;
+};
+
+
+console.log(preOrderTraversal([1,null,2,3])); // [1,2,3]
+console.log(preOrderTraversal([]));
+console.log(preOrderTraversal([1]));
+
+// v2
+
+var preorderTraversal = function(root) {
+    var result = [];
+    
+    traversal(root);
+    
+    function traversal(root){
+        if(root===null){
+            return result;
+        }         
+        else{
+            result.push(root.val);
+            traversal(root.left);
+            traversal(root.right);
+        }   
+    };
+    return result;
+};
+
+
+console.log(preOrderTraversal([1,null,2,3])); // [1,2,3]
+console.log(preOrderTraversal([]));
+console.log(preOrderTraversal([1]));
+
+
+// v3
+
+var preorderTraversal = function(root) {
+    let result = [];
+    dfs(root);
+    
+    function dfs(root) {
+        if(root != null){
+            result.push(root.val);
+            dfs(root.left);
+            dfs(root.right);
+        }
+    }
+    return result;
+};
 
 
 
