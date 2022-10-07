7901774d6c020bb87111f0c31770c9e5411cc94c
Aron Prenovost <Aroniascoder@gmail.com>
2022-10-07 08:15:14 -0700

    Update reverse_nodes_in_k_group.js

--- linked_list/reverse_nodes_in_k_group.js
+++ linked_list/reverse_nodes_in_k_group.js
@@ -155,3 +155,70 @@ previous.next = reverseKGroup(pointer, k);
     
 return result;
 };
+
+
+/* approach #4  (using a stack) */ 
+
+var reverseKGroup = function(head, k) {
+    let stack = [];
+    let newNode = new ListNode(-1);
+    let temp = newNode;
+    
+    while(head) {
+        for(let i = 0; i < k && head; i++) {
+            stack.push(head);
+            head = head.next;
+        }
+        
+        if(stack.length === k) {
+            while(stack.length > 0) {
+                temp.next = stack.pop();
+                temp = temp.next;
+            }
+            temp.next = head;
+        }
+    }
+    return newNode.next;
+};
+
+reverseKGroup([1,2,3,4,5], 2); // [2,1,4,3,5]
+
+/* approach #5 (iteration and recursion) */ 
+
+ /*
+ * 25. Reverse Nodes in k-Group
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const reverseKGroup = (head, k) => {
+  if (head === null) return head
+  // define startNode and endNode
+  let start = head, end = head
+  
+  // return self if linked list's length is smaller than k
+  for (let i = 0; i < k; i++) {
+    if (end === null) return head
+    end = end.next
+  }
+  // reverse from startNode to endNode
+  let newHead = reverse(start, end)
+  // recursive the rest nodes and concat the result
+  start.next = reverseKGroup(end, k)
+  return newHead
+};
+
+// reverse from startNode to endNode
+const reverse = (start, end) => {
+  let [prev, curr] = [null, start]
+  while (curr != end) [curr.next, prev, curr] = [prev, curr, curr.next]
+  return prev
+}
